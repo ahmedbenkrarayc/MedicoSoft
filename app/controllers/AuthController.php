02d02eb94485c6e4bb8e28c633f1d5305e782489
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-09 02:24:13 +0100

    created authcontroller with register get and post actions

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Services\AuthService;
+use Core\View;
+
+class AuthController{
+    private AuthService $authService;
+
+    public function __construct(){
+        $this->authService = new AuthService();
+    }
+
+    public function registerGET(){
+        return View::make('auth/register');
+    }
+
+    public function registerPOST(){
+        $authService = new AuthService();
+        $result = $this->authService->register($_POST);
+        if($result['success'] === true){
+            return View::make('auth/register', ['success' => true]);
+        }
+
+        return View::make('auth/register', ['errors' => $result['errors']]);
+    }
+
+    public function loginGET(){
+        return View::make('auth/login');
+    }
+
+    // public function loginGET(){
+    //     return View::make('auth/login');
+    // }
+}
